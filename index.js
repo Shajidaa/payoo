cb954b37eb233c9ae01bfe3f89f9e0431516351d
Shajidaa <shajidaakterlopa101@gmail.com>
2025-08-22 23:42:22 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// console.log('login');
+const loginBtn=document.getElementById('login-btn');
+
+
+loginBtn.addEventListener('click',function(e){
+     e.preventDefault();
+    
+    console.log(e);
+     console.log('click');
+      
+    const mobileNumber=12345678910;
+    const digit=1234;
+    // console.log(mobileNumber, digit);
+    const inputMobileNumber=document.getElementById('input-mobile-num').value;
+    const pinNumber=document.getElementById('pin-num').value;
+
+     const inputMobileNumberConvert= Number(inputMobileNumber);
+     const pinNumberConvert=Number(pinNumber);
+    
+    if (inputMobileNumberConvert ===mobileNumber && pinNumberConvert=== digit ) {
+        // console.log('All value matches');
+        window.location.href='./home.html'
+        
+    }
+    else{
+        alert('Invalidation');
+        
+    }
+    
+})
