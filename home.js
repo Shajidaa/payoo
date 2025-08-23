9fed0bffb08930b32b09a21de9da4eebc3c4d5bf
Shajidaa <shajidaakterlopa101@gmail.com>
2025-08-23 12:25:02 +0600

    update

--- home.js
+++ home.js
@@ -2,9 +2,9 @@ const btn=document.getElementById('add-money-btn');
 
 const validPin=1234;
 const validAcc=12345678910;
-//function
 
 
+//add money features : 
 btn.addEventListener('click',function () {
     const bank=document.getElementById('bank').value;
     const accountNum=parseInt(document.getElementById('account-num').value);
@@ -28,13 +28,35 @@ btn.addEventListener('click',function () {
    
     
 })
-
+//withdraw money features:
+document.getElementById('withdraw-btn').addEventListener('click',function () {
+    const agentNumber=parseInt(document.getElementById('agent-num').value);
+    const withdrawAmount=parseInt(document.getElementById('withdraw-amount').value);
+      const availableAmount=parseInt(document.getElementById('available-amount').innerText);
+    const pinNum=parseInt(document.getElementById('pin-withdraw').value);
+    // if (agentNumber.length !== 11 ) {
+    //     alert('Please provide valid account number');
+    //     return;
+    // }
+    if (validPin !== pinNum) {
+        alert('Incorrect Pin Number');
+        return;
+    }
+    //cALCULATION
+    const afterWithdrawNewAvailableAmount=availableAmount-withdrawAmount;
+   console.log(withdrawAmount);
+   console.log(availableAmount);
+   
+   console.log(afterWithdrawNewAvailableAmount);
+   
+   document.getElementById('available-amount').innerText=afterWithdrawNewAvailableAmount;
+})
 //toggling 
 
 document.getElementById('add-btn').addEventListener('click',function () {
     document.getElementById('add-money-parent').style.display='block';
     document.getElementById('add-btn').style.border='1px solid blue';
-     document.getElementById('cash-out-btn').style.border='';
+     document.getElementById('cash-out-btn').style.border=document.getElementById('cash-out-btn').style.border;
     document.getElementById('add-text').style.color='blue';
     document.getElementById('cash-out-parent').style.display='none';
 })
